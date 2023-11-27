1cd5b04752b032f645a6b061c0369412bf60e428
Derek Babb <dbabb1@gmail.com>
2023-11-27 09:37:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,44 +13,44 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import AllSorts
 
 def main():
-    random.seed(2020) # This makes sure that the random list will be the same every time.
+  random.seed(2020) # This makes sure that the random list will be the same every time.
 
 
-    numberTerms = 10000
+  numberTerms = 10000
 
-    orderedList = []
-    reversedList = []
-    randomList = []
+  orderedList = []
+  reversedList = []
+  randomList = []
 
-    for i in range(numberTerms):
-        orderedList.append(i)
-        reversedList.insert(0, i)
-        randomList.append(random.randint(1, 10000))
+  for i in range(numberTerms):
+    orderedList.append(i)
+    reversedList.insert(0, i)
+    randomList.append(random.randint(1, 10000))
 
-    # Run each of the sorts in different python sessions.
-    # The sorts are bubbleSort, bubbleSortEarlyExit, selectionSort, insertionSort, and mergeSort
+  # Run each of the sorts in different python sessions.
+  # The sorts are bubbleSort, bubbleSortEarlyExit, selectionSort, insertionSort, and mergeSort
 
-    print("Begin Sorting %d elements." % numberTerms)
+  print("Begin Sorting %d elements." % numberTerms)
 
-    startTime = time.time()
-    AllSorts.bubbleSort(orderedList)
-    endTime = time.time()
-    elapsedTime = endTime - startTime
-    print("Ordered list time: %.5f seconds" % elapsedTime)
+  startTime = time.time()
+  AllSorts.bubbleSort(orderedList)
+  endTime = time.time()
+  elapsedTime = endTime - startTime
+  print("Ordered list time: %.5f seconds" % elapsedTime)
 
-    startTime = time.time()
-    AllSorts.bubbleSort(reversedList)
-    endTime = time.time()
-    elapsedTime = endTime - startTime
-    print("Reversed list time: %.5f seconds" % elapsedTime)
+  startTime = time.time()
+  AllSorts.bubbleSort(reversedList)
+  endTime = time.time()
+  elapsedTime = endTime - startTime
+  print("Reversed list time: %.5f seconds" % elapsedTime)
 
-    startTime = time.time()
-    AllSorts.bubbleSort(randomList)
-    endTime = time.time()
-    elapsedTime = endTime - startTime
-    print("Random list time: %.5f seconds" % elapsedTime)
+  startTime = time.time()
+  AllSorts.bubbleSort(randomList)
+  endTime = time.time()
+  elapsedTime = endTime - startTime
+  print("Random list time: %.5f seconds" % elapsedTime)
 
-    print("Sorting Complete")
+  print("Sorting Complete")
 
 
 if __name__ == '__main__':
